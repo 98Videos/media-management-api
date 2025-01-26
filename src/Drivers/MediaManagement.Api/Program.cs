5cc7d5b2e55f7017fabbbcfb4ca6e3efa9a03b30
Andre Torres <atcorrea17@gmail.com>
2025-01-26 11:02:00 -0300

    fix: program cs startup

--- src/Drivers/MediaManagement.Api/Program.cs
+++ src/Drivers/MediaManagement.Api/Program.cs
@@ -3,10 +3,11 @@ using MediaManagement.Database.DependecyInjection;
 using MediaManagement.S3.DependencyInjection;
 
 using MediaManagement.Api.DependencyInjection;
+using Microsoft.AspNetCore.Http.Features;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Adicionando configura��es de banco de dados PostgreSQL
+// Adicionando configuracoes de banco de dados PostgreSQL
 builder.Services.AddPostgresqlDatabase(builder.Configuration);
 builder.Services.RunDatabaseMigrations(builder.Configuration);
 
@@ -16,11 +17,24 @@ builder.Services.AddS3FileManager(builder.Configuration);
 // Adicionando o caso de uso do v�deo
 builder.Services.AddVideoUseCase();
 
-// Adicionando configura��es b�sicas para a aplica��o
+// Adicionando configuracoes basicas para a aplicacao
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddEndpointsApiExplorer()
+    .AddSwaggerGen();
+
 builder.Services.AddCognitoAuthentication(builder.Configuration);
+
+builder.Services.Configure<FormOptions>(x =>
+{
+    x.ValueLengthLimit = int.MaxValue;
+    x.MultipartBodyLengthLimit = int.MaxValue;
+});
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.Limits.MaxRequestBodySize = int.MaxValue;
+});
 
 // Configurando CORS
 builder.Services.AddCors(options =>
@@ -44,5 +58,5 @@ app.UseCors("CorsPolicy");
 app.UseAuthorization();
 app.MapControllers();
 
-// Iniciando a aplica��o
+// Iniciando a aplicao
 app.Run();
