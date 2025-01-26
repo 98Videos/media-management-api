600a7d8e708edd09b020f02b040828d36f8ce5a5
Angelo Costa Neto <ang.costa.neto@gmail.com>
2025-01-26 13:53:27 -0300

    Correct dependency injection from dbContext

--- src/Drivers/MediaManagement.Api/Program.cs
+++ src/Drivers/MediaManagement.Api/Program.cs
@@ -1,21 +1,26 @@
+using Amazon.S3;
 using MediaManagement.Application.UseCases;
 using MediaManagement.Application.UseCases.Interfaces;
+using MediaManagement.Database.DependecyInjection;
 using MediaManagement.Database.Repositories;
 using MediaManagement.S3.Adapters;
+using MediaManagement.S3.Options;
 using MediaManagementApi.Domain.Repositories;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddAWSService<IAmazonS3>();
+builder.Services.Configure<S3BucketOptions>(builder.Configuration.GetSection("S3BucketOptions"));
+builder.Services.AddPostgresqlDatabase(builder.Configuration);
+builder.Services.RunDatabaseMigrations(builder.Configuration);
+
+builder.Services.AddScoped<IVideoUseCase, VideoUseCase>();
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddScoped<IVideoUseCase, VideoUseCase>();
-builder.Services.AddScoped<IVideoRepository, VideoRepository>();
-builder.Services.AddScoped<IFileRepository, S3FileRepository>();
-
-// Configuração de CORS (permitindo tudo apenas para desenvolvimento)
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("CorsPolicy", builder =>
@@ -28,7 +33,6 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// Configuração do pipeline HTTP
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
