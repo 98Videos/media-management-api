c74c9b3e6acc74b399310341583019acc35654ff
Mauro Tuyoshi Imamura Junior <mauro.imamura@outlook.com>
2025-02-02 14:13:47 -0300

    feat: add zipfile model

---
+++ src/Core/MediaManagementApi.Domain/Entities/ZipFile.cs
@@ -0,0 +1,11 @@
+﻿namespace MediaManagementApi.Domain.Entities;
+
+public record ZipFile
+{
+    private string Identifier { get; set; }
+    private ZipFile() { }
+    public ZipFile(string identifier)
+    {
+        Identifier = identifier;
+    }
+}
