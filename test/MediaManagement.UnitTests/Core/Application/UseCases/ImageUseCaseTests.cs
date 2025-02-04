d5a5aecec91661fe1b8a7ca3673de5a607ac0553
Mauro Tuyoshi Imamura Junior <mauro.imamura@outlook.com>
2025-02-03 23:17:33 -0300

    test: create image usecase and image controller tests

---
+++ test/MediaManagement.UnitTests/Core/Application/UseCases/ImageUseCaseTests.cs
@@ -0,0 +1,34 @@
+﻿using Moq;
+using NUnit.Framework;
+using MediaManagementApi.Domain.Entities;
+using MediaManagement.Application.UseCases.Interfaces;
+using MediaManagementApi.Domain.Repositories;
+using MediaManagement.Application.UseCases;
+
+namespace MediaManagement.UnitTests.Core.Application.UseCases;
+
+[TestFixture]
+public class ImageUseCaseTests
+{
+    private Mock<IFileRepository> _fileRepositoryMock;
+    private IImageUseCase _imageUseCase;
+
+    [SetUp]
+    public void Setup()
+    {
+        _fileRepositoryMock = new Mock<IFileRepository>();
+        _imageUseCase = new ImageUseCase(_fileRepositoryMock.Object);
+    }
+
+    [Test]
+    public async Task GetZipFileAsync_ShouldReturnZipFile()
+    {
+        var zipFile = new ZipFile("test.zip", new MemoryStream());
+        _fileRepositoryMock.Setup(r => r.GetZipFileAsync("user@example.com", "test.zip", It.IsAny<CancellationToken>()))
+            .ReturnsAsync(zipFile);
+
+        var result = await _imageUseCase.DownloadZipFileAsync("user@example.com", "test.zip");
+
+        Assert.AreEqual(zipFile, result);
+    }
+}
---
+++ test/MediaManagement.UnitTests/Drivers/Api/Controllers/ImageControllersTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using MediaManagementApi.Domain.Entities;
+using MediaManagement.Api.Controllers;
+using MediaManagement.Api.Services;
+using MediaManagement.Application.UseCases.Interfaces;
+using MediaManagement.Api.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace MediaManagement.UnitTests.Drivers.Api.Controllers;
+
+[TestFixture]
+public class ImageControllerTests
+{
+    private Mock<IImageUseCase> _imageUseCaseMock;
+    private Mock<ICognitoUserInfoService> _cognitoUserInfoServiceMock;
+
+    [SetUp]
+    public void Setup()
+    {
+        _cognitoUserInfoServiceMock = new Mock<ICognitoUserInfoService>();
+        _imageUseCaseMock = new Mock<IImageUseCase>();
+    }
+
+    [Test]
+    public async Task DownloadZip_ShouldReturnFileResult()
+    {
+        var userInformation = new UserInformation("John Doe", "test@example.com");
+        var zipFile = new ZipFile("test.zip", new MemoryStream(new byte[10]));
+
+        // Configurar o mock do serviço Cognito para retornar o UserInformation
+        _cognitoUserInfoServiceMock.Setup(s => s.GetUserInformationAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(userInformation);
+
+        //Mock
+        _imageUseCaseMock.Setup(u => u.DownloadZipFileAsync(userInformation.Email, "test.zip", It.IsAny<CancellationToken>()))
+            .ReturnsAsync(zipFile);
+
+        // Criar o HttpContext e mockar o Authorization header
+        var httpContext = new DefaultHttpContext();
+        httpContext.Request.Headers["Authorization"] = "Bearer valid.token";
+        var controllerContext = new ControllerContext
+        {
+            HttpContext = httpContext
+        };
+
+        var controller = new ImageController(_cognitoUserInfoServiceMock.Object, _imageUseCaseMock.Object)
+        {
+            ControllerContext = controllerContext // Definir o contexto no controller
+        };
+
+        var result = await controller.DownloadImages("test.zip", It.IsAny<CancellationToken>());
+
+        Console.WriteLine($"Result Type: {result.GetType().Name}");
+
+        if (result is ObjectResult objResult)
+        {
+            Console.WriteLine($"Status Code: {objResult.StatusCode}");
+            Console.WriteLine($"Value: {objResult.Value}");
+        }
+        Assert.IsInstanceOf<FileStreamResult>(result);
+    }
+}
